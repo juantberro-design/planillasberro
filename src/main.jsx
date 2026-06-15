797819f303d08d0be6c3834823c7a50bf9544605
juantberro-design <juantberro@gmail.com>
2026-06-15 14:13:51 -0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import { AuthProvider } from './hooks/useAuth.jsx'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </StrictMode>
+)
